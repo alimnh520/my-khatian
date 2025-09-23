e231ebe54c8b0a4224655b269297592f2704ec18
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-09-24 02:46:04 +0600

    all

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,17 @@
-import { Noto_Sans_Bengali } from "next/font/google";
-import { Rubik } from "next/font/google";
 import "./globals.css";
 
-const noto = Noto_Sans_Bengali({
-  subsets: ["bengali"],
-  weight: ["400", "600"],
-  variable: "--font-noto-bengali",
-});
-const rubic = Rubik({
-  subsets: ['latin'],
-  variable: ['--font-rubic']
-});
+
+export const metadata = {
+  title: "জলিল গাজী || খতিয়ান",
+  description: "This is the online land calculate website",
+  icons: {
+    icon: '/logo.png'
+  }
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="bn" className={`${noto.variable} ${rubic.variable}`}>
+    <html lang="bn">
       <body>
         {children}
       </body>
